e3a96a2c76c4784bad5194b3d077921561e91e67
Robin Métral <robin@metral.ch>
2018-09-18 20:28:51 +0700

    Set up base map and center on Chiang Mai

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// Set the map variable
+const myMap = L.map("mapid");
+
+// Load the basemap
+const myBasemap = L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});
+
+// Add basemap to map id
+myBasemap.addTo(myMap);
+
+// Set view of the map
+myMap.setView([18.7884781, 98.9813945], 14);
