479c09e69adc5166fb7d067ffc2779eb8925bc0e
Robin Métral <robin@metral.ch>
2018-09-19 16:54:11 +0700

    Use custom icon

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,18 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1Ijoicm9iaW5tZXRyYWwiLCJhIjoiY2pkMTI0bWVnMmV6dzM0bnNhZHBvMDBqeiJ9.Z0gZrvkth24hNkLkvRxg-g'
 }).addTo(myMap);
 
+// Create custom icon
+var coffeeIcon = L.icon({
+    iconUrl: "https://robinmetral.github.io/coffee/img/noun_Coffee Bean_1886493-colored.png",
+    shadowUrl: "https://robinmetral.github.io/coffee/img/noun_Coffee Bean_1886493-shadow.png",
+
+    iconSize:     [32, 32], // size of the icon
+    shadowSize:   [32, 32], // size of the shadow
+    iconAnchor:   [16, 16], // point of the icon which will correspond to marker's location
+    shadowAnchor: [16, 16],  // the same for the shadow
+    popupAnchor:  [0, -16] // point from which the popup should open relative to the iconAnchor
+});
+
 // Make an XMLHttpRequest to the JSON data
 const request = new XMLHttpRequest();
 request.open('GET', 'https://robinmetral.github.io/coffee/js/data.json', true);
@@ -20,7 +32,7 @@ request.onload = function() {
     // Print cafe markers
     const cafes = data.cafes.map(cafe => {
         console.log(cafe.name);
-        L.marker([cafe.lat, cafe.lon]).addTo(myMap)
+        L.marker([cafe.lat, cafe.lon], {icon: coffeeIcon}).addTo(myMap)
             .bindPopup(`
             <h1>${cafe.name}</h1>
             <ul>
