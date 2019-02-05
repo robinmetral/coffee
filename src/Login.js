b3b7820bd8b1aed8e61f31fa6329daac6f850610
Robin Métral <robin@metral.ch>
2019-02-05 10:49:27 +0100

    Build login component

---
+++ src/Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Login = (props) => (
+  <div>
+    <h2>Login</h2>
+    <p>Log in with GitHub to manage your online resumes.</p>
+    <button onClick={() => props.authenticate()}>Log in with Github</button>
+  </div>
+);
+
+Login.propTypes = {
+  authenticate: PropTypes.func.isRequired
+};
+
+export default Login;
