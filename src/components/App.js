af87656acac85616aec6f87dd6b3a49e2874c7fb
Robin Métral <robin@metral.ch>
2019-02-05 16:02:51 +0100

    Map through resumes and render in App

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     // take a copy of state to avoid mutations
     const resumes = { ...this.state.fishes }
     // add new resume
-    resumes[`resume${Date.now()}`] = resume
+    resumes[`${Date.now()}`] = resume
     // set state
     this.setState({ resumes })
   }
@@ -20,7 +20,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Resume />
+        {Object.keys(this.state.resumes).map(key => <p>{key}</p>)}
         <Inventory
           addResume={this.addResume}
         />
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,8 +4,7 @@ class Resume extends Component {
   render () {
     return (
       <div>
-        <p>Nothing here yet</p>
-        {/* Check if :resumeId equals to an existing resume */}
+
       </div>
     )
   }
