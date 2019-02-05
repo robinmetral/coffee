2f544fb9d3bbb21fa5a6565841ce3cb1ac407b47
Robin Métral <robin@metral.ch>
2019-02-05 16:40:24 +0100

    Change routing dest in Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React, { Component } from "react"
 class Home extends Component {
   
   handleClick = () => {
-    this.props.history.push("/manage")
+    this.props.history.push("/app")
   }
 
   render() {
