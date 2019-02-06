b4c13368ebb5efa30c8a6b1dc8e59609d1266bab
Robin Métral <robin@metral.ch>
2019-02-06 10:30:57 +0100

    Merge App into Admin and hide resumes from unauth users

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from "react"
-import Admin from "./Admin"
-import Resume from "./Resume"
-import base from "../base"
+import firebase from "firebase"
+import base, { firebaseApp } from "../base"
+import AddResumeForm from "./AddResumeForm"
+import EditResumeForm from "./EditResumeForm"
+import Login from "./Login"
+import Logout from "./Logout"
 
 class App extends Component {
+
   // initialize state
   state = {
-    resumes: {}
+    resumes: {},
+    uid: null,
+    owner: null
   }
 
-  // sync state with firebase
+  // check if logges in
   componentDidMount() {
-    this.ref = base.syncState(`resumes`, {
-      context: this,
-      state: "resumes"
+    firebase.auth().onAuthStateChanged(user => {
+      if(user) {
+        this.authHandler({ user })
+      }
     })
   }
 
@@ -49,15 +56,70 @@ class App extends Component {
     this.setState({ resumes })
   }
 
+  authHandler = async (authData) => {
+    // fetch firebase data
+    const data = await base.fetch(`/`, { context: this })
+    // in case owner isn't set, set as my gh uid
+    if(!data.owner) {
+      await base.post(`/owner`, {
+        data: "YQogwrdn45fUQ7tGAESDraONoyN2"
+      })
+    }
+    // set logged in user to state
+    this.setState({
+      uid: authData.user.uid,
+      owner: data.owner
+    })
+    // sync resumes in state
+    this.ref = base.syncState(`resumes`, {
+      context: this,
+      state: "resumes"
+    })
+  }
+
+  authenticate = () => {
+    const authProvider = new firebase.auth.GithubAuthProvider()
+    firebaseApp.auth().signInWithPopup(authProvider).then(this.authHandler)
+  }
+
+  logout = async () => {
+    // log out on firebase
+    await firebase.auth().signOut()
+    // clear state
+    this.setState({ uid: null })
+  }
+
   render() {
+    // check if logged in
+    if(!this.state.uid) {
+      return <Login authenticate={this.authenticate} />
+    }
+
+    // check if owner
+    if(this.state.uid !== this.state.owner) {
+      return (
+        <div>
+          <p>Sorry, you're not the owner.</p>
+          <Logout logout={this.logout} />
+        </div>
+      )
+    }
+
+    // otherwise render admin
     return (
       <div>
-        <Admin
-          addResume={this.addResume}
+        <h2>Admin</h2>
+        { Object.keys(this.state.resumes).map( key => (
+        <EditResumeForm
+          key={key}
+          index={key}
+          resume={this.state.resumes[key]}
           updateResume={this.updateResume}
           deleteResume={this.deleteResume}
-          resumes={this.state.resumes}
-        />
+        /> 
+        ))}
+        <AddResumeForm addResume={this.addResume} />
+        <Logout logout={this.logout} />
       </div>
     )
   }
