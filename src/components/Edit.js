65855a663fa1ef9207e03cba487f5ddfb78a9b19
Robin Métral <robin@metral.ch>
2019-02-07 11:34:26 +0100

    Use formik on Edit form to handle errors and validation

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,5 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { Formik, Form, Field, ErrorMessage } from "formik"
 
 class Edit extends React.Component {
 
@@ -13,8 +14,8 @@ class Edit extends React.Component {
     })
   }
 
+  /*
   handleChange = (event) => {
-    // update that resume
     // take a copy of current resume
     const updatedResume = {
       ...this.props.resume,
@@ -22,8 +23,19 @@ class Edit extends React.Component {
     }
     this.props.updateResume(this.props.index, updatedResume)
   }
+  */
+
+  handleChange = event => {
+    const { name, value } = event.target
+    const updatedResume = {
+      ...this.props.resume,
+      [name]: value
+    }
+    this.props.updateResume(this.props.index, updatedResume)
+  }
 
   render() {
+    /*
     const { resume } = this.props
     return (
       <div className="resume-edit">
@@ -31,8 +43,38 @@ class Edit extends React.Component {
         <textarea name="summary" onChange={this.handleChange} value={resume.summary} />
         <button onClick={() => this.props.deleteResume(this.props.index)}>Remove Resume</button>
       </div>
-      )
-}
+    )
+    */
+    return (
+      <Formik
+        initialValues={{
+          name: this.props.resume.name,
+          summary: this.props.resume.summary
+        }}
+        enableReinitialize={true}
+        validate={values => {
+          let errors = {}
+          if (!values.name) {
+            errors.name = "The name is required."
+          }
+          if (values.summary.length < 10) {
+            errors.summary = "The summary is too short."
+          }
+          return errors
+        }}
+      >
+        <Form>
+          <Field type="text" name="name" onChange={this.handleChange} />
+          <ErrorMessage name="name" component="div" />
+          <Field type="textarea" name="summary" onChange={this.handleChange} />
+          <ErrorMessage name="summary" component="div" />
+          <button type="delete" onClick={() => this.props.deleteResume(this.props.index)}>
+            Delete
+          </button>
+        </Form>
+      </Formik>
+    )
+  }
 }
 
 export default Edit
