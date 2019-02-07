1adff7cc65a099f08f0d9c3c8ed8e290bf8979be
Robin Métral <robin@metral.ch>
2019-02-07 11:41:58 +0100

    Add add and edit features to external routes

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,12 +1,16 @@
 import React from "react"
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 import App from "./App"
+import Add from "./Add"
+import Edit from "./Edit"
 import View from "./View"
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path={`${process.env.PUBLIC_URL}/`} component={App} />
+      <Route path={`${process.env.PUBLIC_URL}/add`} component={Add} />
+      <Route path={`${process.env.PUBLIC_URL}/edit/:resumeId`} component={Edit} />
       <Route path={`${process.env.PUBLIC_URL}/:resumeId`} component={View} />
     </Switch>
   </BrowserRouter>
