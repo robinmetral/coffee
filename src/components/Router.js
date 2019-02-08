cdc8dc3ea730aea9bca76a0f5469a625cc77c27f
Robin Métral <robin@metral.ch>
2019-02-08 11:53:28 +0100

    Fix import path in Router

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { BrowserRouter, Route, Switch } from "react-router-dom"
-import App from "components/App"
+import App from "./App"
 
 const Router = () => (
   <BrowserRouter>
