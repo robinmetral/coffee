eb0901ddd9fdfdc1d993eba4464773d28c5e19bf
Robin Métral <robin@metral.ch>
2019-02-08 10:14:30 +0100

    Route to App

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,15 +1,11 @@
 import React from "react"
 import { BrowserRouter, Route, Switch } from "react-router-dom"
-import Map from "components/Map"
-import Add from "components/Add"
-import Edit from "components/Edit"
+import App from "components/App"
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path={`${process.env.PUBLIC_URL}/`} component={Map} />
-      <Route path={`${process.env.PUBLIC_URL}/add`} component={Add} />
-      <Route path={`${process.env.PUBLIC_URL}/edit/:cafeId`} component={Edit} />
+      <Route exact path={`${process.env.PUBLIC_URL}/`} component={App} />
     </Switch>
   </BrowserRouter>
 )
