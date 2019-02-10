c90f2f58cc480377a425158ba880871b1b0008c0
Robin Métral <robin@metral.ch>
2019-02-10 22:02:41 +0100

    Opt in to using CRA's service worker

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react"
 import { render } from "react-dom"
 import Router from "./components/Router"
+import * as serviceWorker from "./serviceWorker"
 
 render(<Router />, document.getElementById("root"))
+
+serviceWorker.register()
