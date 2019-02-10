c8e6cf99527d051655c8757c7883a6fe7374e7d3
Robin Métral <robin@metral.ch>
2019-02-10 08:20:02 +0100

    Map through firebase cafes and display markers

--- src/components/Markers.js
+++ src/components/Markers.js
@@ -5,15 +5,22 @@ class Markers extends Component {
   render () {
     // if the request has loaded
     if(Object.getOwnPropertyNames(this.props.cafes).length > 0) {
+      const { cafes } = this.props
       return (
-        <Marker position={[52.51306, 13.43174]}>
-          <Popup>{this.props.cafes["2640326880"].comment}</Popup>
-        </Marker>
-        )
+        <>
+        {
+          Object.keys(cafes).map( cafe => (
+            <Marker position={cafes[cafe].coordinates}>
+              <Popup>{cafes[cafe].comment}</Popup>
+            </Marker>
+          ))
+        }
+        </>
+      )
     }
     return (
       <p>Loading...</p>
-    )
+      )
   }
 }
 
