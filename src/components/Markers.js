5b39d1dc7c6d50a906153a5e10b13ccf812b51ce
Robin Métral <robin@metral.ch>
2019-02-10 16:56:29 +0100

    Use a custom icon for cafes

--- src/components/Markers.js
+++ src/components/Markers.js
@@ -1,5 +1,16 @@
 import React, { Component } from "react"
 import { Marker, Popup } from "react-leaflet"
+import L from "leaflet"
+
+const CoffeeBean = new L.Icon({
+  iconUrl: require("../assets/icon-coffee-bean.png"),
+  shadowUrl: require("../assets/icon-coffee-bean-shadow.png"),
+  iconSize:     [32, 32], // size of the icon
+  shadowSize:   [32, 32], // size of the shadow
+  iconAnchor:   [16, 16], // point of the icon which will correspond to marker's location
+  shadowAnchor: [16, 16], // the same for the shadow
+  popupAnchor:  [2, -18]  // point from which the popup should open relative to the iconAnchor 
+})
 
 class Markers extends Component {
   render () {
@@ -13,10 +24,11 @@ class Markers extends Component {
             <Marker
               key={cafes[cafe].osm}
               position={cafes[cafe].coordinates}
+              icon={CoffeeBean}
             >
               <Popup>{cafes[cafe].comment}</Popup>
             </Marker>
-          ))
+            ))
         }
         </>
       )
