b5277d258cc30e63cc37171887e123c7c5fbcc37
Robin Métral <robin@metral.ch>
2019-02-10 16:47:57 +0100

    Change map tiles and update attribution

--- src/components/Map.js
+++ src/components/Map.js
@@ -11,8 +11,8 @@ class Map extends Component {
     return (
       <LeafletMap center={[52.5134, 13.4225]} zoom={13}>
         <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          attribution="Map data &copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap contributors</a>, Mapbox Light tiles &copy; <a href='https://www.mapbox.com/about/maps/'>Mapbox</a>, coffee bean by Gyeong Seon Hong of <a href='https://thenounproject.com/icon/1886493/'>The Noun Project</a>"
+          url="https://api.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoicm9iaW5tZXRyYWwiLCJhIjoiY2pkMTI0bWVnMmV6dzM0bnNhZHBvMDBqeiJ9.Z0gZrvkth24hNkLkvRxg-g"
         />
         <Markers cafes={this.props.cafes} />
       </LeafletMap>
