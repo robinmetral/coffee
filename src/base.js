dbd517b70bf80f9d86f11b9ff5ffb966b5b8d9ef
Robin Métral <robin@metral.ch>
2019-02-10 15:45:51 +0100

    Import firebase according to console warning message

--- src/base.js
+++ src/base.js
@@ -1,5 +1,7 @@
 import Rebase from "re-base"
-import firebase from "firebase"
+import firebase from "firebase/app"
+import "firebase/auth"
+import "firebase/database"
 
 const firebaseApp = firebase.initializeApp (
   {
