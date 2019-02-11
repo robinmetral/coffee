612e270898a18ee33a548f2a941381d7769cf1e6
Robin Métral <robin@metral.ch>
2019-02-11 17:23:51 +0100

    Build ViewCafe component to render clicked-on cafe when logged out

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react"
 
 import Auth from "./Auth"
+import ViewCafe from "./ViewCafe"
 
 class Home extends Component {
   render() {
     return (
       <div>
         <h1>Carte des cafés de Robin ☕</h1>
-        <p>Bienvenue !</p>
+        <ViewCafe
+          cafe={this.props.cafe}
+        />
         <Auth
           uid={this.props.uid}
           owner={this.props.owner}
---
+++ src/components/ViewCafe.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react"
+
+class ViewCafe extends Component {
+  render() {
+    // is no cafe is passed, no cafe has been clicked
+    if(!this.props.cafe) {
+      return <p>Cliquez sur un grain de café pour voir ses détails</p>
+    }
+
+    // if a cafe has been clicked, render
+    const { name, rating, comment, clara, robin, laptop, hours, url, osm } = this.props.cafe
+    return (
+      <>
+        <h2>{ name } &middot; { rating }</h2>
+        <p>{ comment }</p>
+        <ul>
+          <li>Café pour Robin : { robin }</li>
+          <li>Café pour Clara : { clara }</li>
+          <li>Laptops ? { laptop }</li>
+          <li>Heures d'ouverture : { hours }</li>
+        </ul>
+        <p><a href={ url }>Site web</a> &middot; <a href={`https://www.openstreetmap.org/node/${ osm }`}>OpenStreetMap</a></p>
+      </>
+    )
+  }
+}
+
+export default ViewCafe
