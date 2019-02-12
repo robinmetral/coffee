7644d115535e0e363f0598d96acee3a48a4ae603
Robin Métral <robin@metral.ch>
2019-02-12 15:52:21 +0100

    Change panel width to golden ratio

--- src/containers/PanelLayout.js
+++ src/containers/PanelLayout.js
@@ -2,15 +2,15 @@ import React from "react"
 import styled from "styled-components"
 
 const Container = styled.div`
-  width: 50vw;
+  width: 38vw;
   height: 100vh;
   z-index: 1000;
   background: white;
   position: absolute;
   top: 0;
-  right: -50vw;
+  right: -38vw;
   transition: transform ease 500ms;
-  transform: ${props => (props.panel === "open") ? "translate3d(-50vw, 0, 0)" : "none"};
+  transform: ${props => (props.panel === "open") ? "translate3d(-38vw, 0, 0)" : "none"};
 `
 
 const PanelLayout = (props) => (
