289072ab28c9f4b83a58a1b6c2061224489b20d3
Robin Métral <robin@metral.ch>
2019-02-13 20:10:45 +0100

    Add grid gap

--- src/components/styled/StyledForm.js
+++ src/components/styled/StyledForm.js
@@ -3,6 +3,7 @@ import styled from "styled-components"
 const StyledForm = styled.fieldset`
   display: grid;
   grid-template-columns: 1fr auto auto;
+  grid-gap: 0.5rem;
 `
 
 export default StyledForm
