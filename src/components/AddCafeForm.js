b1b4fcfd0f941d5a057f5e4efcb5683dbc2b6169
Robin Métral <robin@metral.ch>
2019-02-13 19:30:48 +0100

    Remove extraneous div by styling fieldset directly

--- src/components/AddCafeForm.js
+++ src/components/AddCafeForm.js
@@ -26,67 +26,65 @@ class AddCafeForm extends Component {
       >
         {({ isSubmitting }) => (
         <Form>
-          <fieldset>
+          <StyledForm>
             <legend>Ajouter un café</legend>
 
-            <StyledForm>
-              <label>
-                <div>Nom</div>
-                <ErrorMessage name="name" component="div" />
-                <Field
-                  type="text"
-                  name="name"
-                  placeholder="Nom"
-                />
-              </label>
+            <label>
+              <div>Nom</div>
+              <ErrorMessage name="name" component="div" />
+              <Field
+                type="text"
+                name="name"
+                placeholder="Nom"
+              />
+            </label>
 
-              <label>
-                <div>ID OpenStreetMap</div>
-                <ErrorMessage name="osm" component="div" />
-                <Field
-                  type="number"
-                  name="osm"
-                  placeholder="ID OSM"
-                />
-              </label>
+            <label>
+              <div>ID OpenStreetMap</div>
+              <ErrorMessage name="osm" component="div" />
+              <Field
+                type="number"
+                name="osm"
+                placeholder="ID OSM"
+              />
+            </label>
 
-              <label>
-                <div><span role="img" aria-label="laptop emoji">💻</span></div>
-                <ErrorMessage name="laptop" component="div" />
-                <Field
-                  type="checkbox"
-                  name="laptop"
-                  value="true"
-                />
-              </label>
+            <label>
+              <div><span role="img" aria-label="laptop emoji">💻</span></div>
+              <ErrorMessage name="laptop" component="div" />
+              <Field
+                type="checkbox"
+                name="laptop"
+                value="true"
+              />
+            </label>
 
-              <label>
-                <div><span role="img" aria-label="star emoji">⭐</span></div>
-                <ErrorMessage name="rating" component="div" />
-                <Field component="select" name="rating">
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                  <option value="5">5</option>
-                </Field>
-              </label>
+            <label>
+              <div><span role="img" aria-label="star emoji">⭐</span></div>
+              <ErrorMessage name="rating" component="div" />
+              <Field component="select" name="rating">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </Field>
+            </label>
 
-              <label>
-                <div>Commentaire</div>
-                <ErrorMessage name="comment" component="div" />
-                <Field
-                  component="textarea"
-                  name="comment"
-                  placeholder="Commentaire"
-                />
-              </label>
+            <label>
+              <div>Commentaire</div>
+              <ErrorMessage name="comment" component="div" />
+              <Field
+                component="textarea"
+                name="comment"
+                placeholder="Commentaire"
+              />
+            </label>
 
-              <button type="submit" disabled={isSubmitting}>
-                Ajouter
-              </button>
-            </StyledForm>
-          </fieldset>
+            <button type="submit" disabled={isSubmitting}>
+              Ajouter
+            </button>
+          </StyledForm>
         </Form>
         )}
       </Formik>
--- src/components/styled/StyledForm.js
+++ src/components/styled/StyledForm.js
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 
-const StyledForm = styled.div`
+const StyledForm = styled.fieldset`
   display: grid;
 `
 
