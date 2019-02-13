3cd0671a3b50867507a8477badeddec1612ffc34
Robin Métral <robin@metral.ch>
2019-02-13 20:36:57 +0100

    Normalize form inputs styling

--- src/components/styled/StyledInput.js
+++ src/components/styled/StyledInput.js
@@ -2,6 +2,13 @@ import styled from "styled-components"
 
 const StyledInput = styled.label`
   display: grid;
+  input, textarea, select {
+    display: block; // teaxtarea is inline-block by default
+    font-family: inherit; // not inherited by default
+    font-size: 100%; // not inherited by default
+    box-sizing: border-box; // harmonize box sizing
+    width: 100%;
+  }
 `
 
 export default StyledInput
