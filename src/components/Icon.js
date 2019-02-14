709c755aeeb3af1ae8a56417e6befd5ed5663999
Robin Métral <robin@metral.ch>
2019-02-14 15:49:52 +0100

    Render a button span or a conditionally with Icon component

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,27 +1,69 @@
-import React from "react"
+import React, { Component } from "react"
 import styled from "styled-components"
 
-const StyledIcon = styled.button`
+const Button = styled.button`
   background: none;
   border: none;
-  font-size: 2rem;
+  cursor: pointer;
   &:hover {
     filter: brightness(75%);
   }
 `
 
-const Icon = (props) => (
-  <StyledIcon
-    onClick={props.action}
-    title={props.title}
+const Link = styled.a`
+  text-decoration: none;
+  &:hover {
+    filter: brightness(75%);
+  }
+`
+
+const StyledEmoji = styled.span`
+  font-size: 2rem;
+`
+
+const Emoji = (props) => (
+  <StyledEmoji
+    role="img"
+    title={ props.title }
+    aria-label={`${ props.description } emoji`}
   >
-    <span
-      role="img"
-      aria-label={`${props.description} emoji`}
-    >
-      {props.code}
-    </span>
-  </StyledIcon>
+    { props.code }
+  </StyledEmoji>
 )
+
+class Icon extends Component {
+  render() {
+
+    // desctructure
+    const { code, description, title, action, url } = this.props
+
+    // if button
+    if (action) {
+      return (
+        <Button
+          onClick={action}
+          title={title}
+        >
+          <Emoji code={code} description={description} />
+        </Button>
+        )
+    }
+
+    // if link
+    if (url) {
+      return (
+        <Link
+          href={url}
+          title={title}
+        >
+          <Emoji code={code} description={description} />
+        </Link>
+        )
+    }
+
+    // otherwise render static icon
+    return <Emoji code={code} description={description} title={title} />
+  }
+}
 
 export default Icon
