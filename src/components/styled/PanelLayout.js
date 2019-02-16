b4f939832526e92afb562c4feec4d52e7b3811b7
Robin Métral <robin@metral.ch>
2019-02-16 19:02:32 +0100

    Change panel sizing for mobile-first layout

--- src/components/styled/PanelLayout.js
+++ src/components/styled/PanelLayout.js
@@ -3,15 +3,15 @@ import styled from "styled-components"
 
 const Container = styled.div`
   // position and animation
-  width: 38vw;
+  width: 100vw;
   height: 100vh;
   z-index: 1000;
   background: white;
   position: absolute;
   top: 0;
-  right: -38vw;
+  right: -100vw;
   transition: transform ease 500ms;
-  transform: ${props => (props.panel === "open") ? "translate3d(-38vw, 0, 0)" : "none"};
+  transform: ${props => (props.panel === "open") ? "translate3d(-100vw, 0, 0)" : "none"};
   box-shadow: ${props => (props.panel === "open") ? "-5px 0 25px 0 rgba(0, 0, 0, 0.3)" : "none"};
   // children
   display: grid;
