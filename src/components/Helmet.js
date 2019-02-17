719fbff436bc338df607f6d6fdbef6e25494e496
Robin Métral <robin@metral.ch>
2019-02-17 12:25:53 +0100

    Specify favicon type

--- src/components/Helmet.js
+++ src/components/Helmet.js
@@ -7,7 +7,7 @@ const Helmet = (props) => (
   <ReactHelmet>
     <html lang="fr" />
     <meta charSet="utf-8" />
-    <link rel="shortcut icon" href={favicon} />
+    <link rel="icon" type="image/png" href={favicon} />
     <link rel="manifest" href={`${process.env.PUBLIC_URL}/manifest.json`} />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="theme-color" content="#6F4E37" />
