bf596b7a6c6fd6aa22c9e7a36f9e4efbdf832df1
Robin Métral <robin@metral.ch>
2019-02-17 11:57:16 +0100

    Dynamically set page title based on state

--- src/components/Helmet.js
+++ src/components/Helmet.js
@@ -1,11 +1,9 @@
 import React from "react"
 import { Helmet as ReactHelmet } from "react-helmet"
 
-const Helmet = () => (
-  <ReactHelmet
-    titleTemplate="%s &middot; Les cafés de Robin"
-    defaultTitle="Les cafés de Robin"
-  >
+const Helmet = (props) => (
+  <ReactHelmet>
+    <title>{ props.title === "" ? `Les cafés de Robin` : `${props.title} | Les cafés de Robin` }</title>
     <meta name="description" content="Une app React pour partager les cafés que j'aime !" />
   </ReactHelmet>
 )
