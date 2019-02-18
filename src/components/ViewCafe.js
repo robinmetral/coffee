5a8bda1bfd5e47e770ed8fa3b2c502133bdaeaba
Robin Métral <robin@metral.ch>
2019-02-18 12:22:40 +0100

    Remove Icon styled component

--- src/components/ViewCafe.js
+++ src/components/ViewCafe.js
@@ -5,24 +5,21 @@ import CafeLayout from "./styled/CafeLayout"
 import Heading from "./styled/Heading"
 import Emoji from "./Emoji"
 
-const IconsList = styled.ul`
+const List = styled.ul`
   border-top: 1px solid lightgray;
   list-style-type: none;
-  padding: 0;
+  padding: 1rem 0 0 0;
   display: flex;
   flex-direction: column;
   li {
     display: flex;
-  }
-`
-
-const Icon = styled.li`
-  margin-top: 0.5rem;
-  a {
-    text-decoration: none;
-    color: inherit;
-    &:hover {
-      text-decoration: underline;
+    margin-top: 0.5rem;
+    a {
+      text-decoration: none;
+      color: inherit;
+      &:hover {
+        text-decoration: underline;
+      }
     }
   }
 `
@@ -39,34 +36,34 @@ class ViewCafe extends Component {
       <CafeLayout>
         <Heading>{ name }</Heading>
         { [...Array(rating)].map((star, key) =>
-          <span role="img" aria-label="star emoji" key={key}>&#x2b50;</span>
+        <span role="img" aria-label="star emoji" key={key}>&#x2b50;</span>
         )}
         <p>{ comment }</p>
-        <IconsList>
+        <List>
           { laptop && (
-          <Icon>
+          <li>
             <Emoji unicode="1f4bb" alt="Laptop Computer" />
             Recommandé pour travailler
-          </Icon>
+          </li>
           ) }
           { url && (
-          <Icon>
+          <li>
             <Emoji unicode="1f517" alt="Link" />
             <a href={ url }>{ url }</a>
-          </Icon>
+          </li>
           ) }
-          <Icon>
+          <li>
             <Emoji unicode="1f30d" alt="Globe Showing Europe-Africa" />
             <a href={`https://www.openstreetmap.org/node/${ osm }`}>Ouvrir dans OpenStreetMap</a>
-          </Icon>
-          <Icon>
+          </li>
+          <li>
             <Emoji unicode="1f557" alt="Eight O'Clock" />
             { hours }
-          </Icon>
-        </IconsList>
+          </li>
+        </List>
       </CafeLayout>
-    )
-  }
+      )
+}
 }
 
 export default ViewCafe
