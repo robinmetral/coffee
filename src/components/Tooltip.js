3aeddcea154c1ab4a849222e0995ce33e3e8d981
Robin Métral <robin@metral.ch>
2019-02-22 14:45:11 +0100

    Style café name in Tooltip

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,11 +1,17 @@
 import React from "react"
 import { Tooltip as LeafletTooltip } from "react-leaflet"
+import styled from "styled-components"
 
 import Rating from "./Rating"
 
+const Name = styled.span`
+  font-size: 1rem;
+  font-weight: 500;
+`
+
 const Tooltip = (props) => (
   <LeafletTooltip direction="top">
-    { props.name }<br />
+    <Name>{ props.name }</Name><br />
     <Rating rating={ props.rating } />
   </LeafletTooltip>
 )
