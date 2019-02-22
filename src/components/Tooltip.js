5580feb353750716f39a12f2cac60445b2fa1b9a
Robin Métral <robin@metral.ch>
2019-02-22 14:41:51 +0100

    Render star rating in tooltip with Rating component

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,9 +1,12 @@
 import React from "react"
 import { Tooltip as LeafletTooltip } from "react-leaflet"
 
+import Rating from "./Rating"
+
 const Tooltip = (props) => (
   <LeafletTooltip direction="top">
-    { props.name }
+    { props.name }<br />
+    <Rating rating={ props.rating } />
   </LeafletTooltip>
 )
 
