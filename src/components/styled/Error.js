c1de30b809470cb4d03314cd26e9cba5acfc8b5c
Robin Métral <robin@metral.ch>
2019-02-23 23:05:02 +0100

    Style Error component as red-bordered paragraph

---
+++ src/components/styled/Error.js
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+const Error = styled.p`
+  border: 2px solid hsl(360, 75%, 75%);
+  color: hsl(360, 75%, 55%);
+  border-radius: 4px;
+  padding: 1rem;
+`
+
+export default Error
