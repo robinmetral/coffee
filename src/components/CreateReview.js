0271cba5b717dd54e3abfae7b0337e6ca797e559
Robin Métral <robin@metral.ch>
2019-02-27 21:33:44 +0100

    Transform AddCafeForm into CreateReview with Grommet

---
+++ src/components/CreateReview.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { Form, FormField, Button } from "grommet";
+
+class CreateReview extends Component {
+  render() {
+    return (
+      <Form>
+        <FormField label="Rating">
+          <RadioButtonGroup name="rating" options={[1, 2, 3, 4, 5]} />
+        </FormField>
+        <FormField label="Review">
+          <TextArea value="Write a review (optional)" />
+        </FormField>
+        <Button type="submit" primary label="Submit" />
+      </Form>
+    );
+  }
+}
+
+export default CreateReview;
