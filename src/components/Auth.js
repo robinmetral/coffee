8da00bc29f45af0e6bfa50b4ea855bfcccf7c320
Robin Métral <robin@metral.ch>
2019-02-28 08:58:47 +0100

    Build sign up form

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,15 +1,32 @@
 import React, { Component } from "react";
 import { Form, FormField, TextInput } from "grommet";
+import firebase from "firebase/app";
+import "firebase/auth";
 
 class Auth extends Component {
+  emailRef = React.createRef();
+  passwordRef = React.createRef();
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const email = this.emailRef.current.value;
+    const password = this.passwordRef.current.value;
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch(function(error) {
+        console.log(`Error ${error.code}: ${error.message}`);
+      });
+  };
+
   render() {
     return (
-      <Form>
+      <Form onSubmit={this.handleSubmit}>
         <FormField label="email">
-          <TextInput />
+          <TextInput type="email" ref={this.emailRef} />
         </FormField>
         <FormField label="password">
-          <TextInput />
+          <TextInput type="password" ref={this.passwordRef} />
         </FormField>
       </Form>
     );
