f04955da8f6e6414a33bf6e319c619f7534e2946
Robin Métral <robin@metral.ch>
2019-03-01 15:33:02 +0100

    Transform Cafe into a Stateless Functional Component

--- src/components/Cafe.js
+++ src/components/Cafe.js
@@ -1,35 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
 import { Box, Heading, Button } from "grommet";
 import { FormClose } from "grommet-icons";
 import Info from "./Info";
 import Reviews from "./Reviews";
 
-class Cafe extends Component {
-  render() {
-    // is no cafe is passed, no cafe has been clicked
-    if (!this.props.cafe) {
-      return null;
-    }
-    return (
-      <Box width="medium" elevation="large" fill="vertical" overflow="auto">
-        <Box direction="row" align="center" justify="between">
-          <Heading size="small" margin={{ left: "small" }}>
-            {this.props.cafe.properties.name}
-          </Heading>
-          <Button icon={<FormClose />} onClick={this.props.togglePanel} />
-        </Box>
-        <Box pad="small">
-          <Info cafe={this.props.cafe} />
-        </Box>
-        <Box pad="small">
-          <Reviews
-            cafe={this.props.cafe}
-            createReview={this.props.createReview}
-          />
-        </Box>
-      </Box>
-    );
+const Cafe = props => {
+  // is no cafe is passed, no cafe has been clicked
+  if (!props.cafe) {
+    return null;
   }
-}
+  return (
+    <Box width="medium" elevation="large" fill="vertical" overflow="auto">
+      <Box direction="row" align="center" justify="between">
+        <Heading size="small" margin={{ left: "small" }}>
+          {props.cafe.properties.name}
+        </Heading>
+        <Button icon={<FormClose />} onClick={props.togglePanel} />
+      </Box>
+      <Box pad="small">
+        <Info cafe={props.cafe} />
+      </Box>
+      <Box pad="small">
+        <Reviews cafe={props.cafe} createReview={props.createReview} />
+      </Box>
+    </Box>
+  );
+};
 
 export default Cafe;
