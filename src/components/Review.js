f5444021c06dae31ab93160b0407daa80bb7bc23
Robin Métral <robin@metral.ch>
2019-03-01 09:41:03 +0100

    Add date formatting

--- src/components/Review.js
+++ src/components/Review.js
@@ -3,11 +3,22 @@ import { Box, Text } from "grommet";
 
 const Review = props => {
   const { createdAt, rating, text } = props;
-  const date = new Date(createdAt).toDateString();
+  const timestamp = new Date(createdAt);
+  const date = timestamp.toLocaleDateString("en-GB", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  });
+  const time = timestamp.toLocaleTimeString("en-GB", {
+    hour: "numeric",
+    minute: "numeric"
+  });
   return (
     <Box>
+      <Text>Someone | {rating}</Text>
       <Text>
-        Rated {rating} by Someone on {date}
+        {date} at {time}
       </Text>
       <Text>{text}</Text>
     </Box>
