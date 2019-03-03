e3443280b02284c7a8101a1f0caacfd1f12ef429
Robin Métral <robin@metral.ch>
2019-03-03 17:58:17 +0100

    Remove unused prop from grommet Box

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -28,7 +28,6 @@ class Panel extends Component {
             elevation="large"
             fill="vertical"
             overflow="auto"
-            flex="false"
             pad={{ horizontal: "medium" }}
           >
             <Cafe
