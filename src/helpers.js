3acc45384c340bb712a70e4cc6035e8bd9df08bd
Robin Métral <robin@metral.ch>
2019-03-03 16:08:36 +0100

    Handle no reviews in averageRating

--- src/helpers.js
+++ src/helpers.js
@@ -84,6 +84,7 @@ export const formatAddress = cafe => {
 };
 
 export const averageRating = reviews => {
+  if (!reviews) return null;
   // build ratings array
   const ratings = [];
   Object.keys(reviews).map(id => {
