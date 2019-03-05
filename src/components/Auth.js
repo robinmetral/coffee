04df8c5db4cc6216328674b3d595938ccfc8836f
Robin Métral <robin@metral.ch>
2019-03-05 10:43:50 +0100

    Transform Auth into a SFC

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,20 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import Login from "./Login";
 import Logout from "./Logout";
 
-class Auth extends Component {
-  render() {
-    const { user } = this.props;
-    return (
-      <>
-        {user ? (
-          <Logout user={user} logout={this.props.logout} />
-        ) : (
-          <Login login={this.props.login} />
-        )}
-      </>
-    );
-  }
-}
+const Auth = props => {
+  return (
+    <>
+      {props.user ? (
+        <Logout user={props.user} logout={props.logout} />
+      ) : (
+        <Login login={props.login} />
+      )}
+    </>
+  );
+};
 
 export default Auth;
