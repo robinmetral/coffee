4f91360d8c0278ae8781bc4e0e57dbf778748217
Robin Métral <robin@metral.ch>
2019-03-07 12:51:28 +0100

    Render Login button in Heading

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Box, Heading as GrommetHeading, Button } from "grommet";
-import { Close } from "grommet-icons";
+import { Close, Login } from "grommet-icons";
 
 const Heading = props => (
   <Box direction="row" align="center" justify="between">
     <GrommetHeading size="small" margin={{ bottom: "small" }}>
       {props.title}
     </GrommetHeading>
-    <Button icon={<Close />} onClick={props.togglePanel} />
+    <Box direction="row">
+      <Button icon={<Login />} />
+      <Button icon={<Close />} onClick={props.togglePanel} />
+    </Box>
   </Box>
 );
 
