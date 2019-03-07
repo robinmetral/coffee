6b652e4a857d03684a398c21d88170b79f3298f6
Robin Métral <robin@metral.ch>
2019-03-07 12:39:03 +0100

    Update review on Submit

--- src/components/UpdateReview.js
+++ src/components/UpdateReview.js
@@ -4,20 +4,32 @@ import { Send, Close } from "grommet-icons";
 import RatingInput from "./RatingInput";
 
 class UpdateReview extends Component {
+  state = {
+    rating: this.props.review.rating,
+    text: this.props.review.text
+  };
+
   handleChange = (name, value) => {
+    // update state
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSubmit = () => {
     // update review
     const review = {
       ...this.props.review,
-      [name]: value,
+      rating: this.state.rating,
+      text: this.state.text,
       updatedAt: Date.now()
     };
     this.props.updateReview(this.props.id, review);
+    this.props.toggleEdit();
   };
 
-  handleSubmit = () => {};
-
   render() {
-    const { rating, text } = this.props.review;
+    const { rating, text } = this.state;
     return (
       <Form onSubmit={this.handleSubmit}>
         <RatingInput rating={rating} handleChange={this.handleChange} />
@@ -29,7 +41,11 @@ class UpdateReview extends Component {
           onChange={e => this.handleChange(e.target.name, e.target.value)}
         />
         <Box direction="row">
-          <Button icon={<Send />} label="Submit" onSubmit={this.handleSubmit} />
+          <Button
+            type="submit"
+            icon={<Send />}
+            label="Submit"
+          />
           <Button
             icon={<Close />}
             label="Cancel"
