aaa0845b594c5c8dfbfa70b6a76ad32eb4c0c081
Robin Métral <robin@metral.ch>
2019-03-07 12:49:55 +0100

    Use different Close icon in Heading

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Box, Heading as GrommetHeading, Button } from "grommet";
-import { FormClose } from "grommet-icons";
+import { Close } from "grommet-icons";
 
 const Heading = props => (
   <Box direction="row" align="center" justify="between">
     <GrommetHeading size="small" margin={{ bottom: "small" }}>
       {props.title}
     </GrommetHeading>
-    <Button icon={<FormClose />} onClick={props.togglePanel} />
+    <Button icon={<Close />} onClick={props.togglePanel} />
   </Box>
 );
 
