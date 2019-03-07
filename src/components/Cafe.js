a7991a013fc81c3d15b2793738e98d37e0f94b32
Robin Métral <robin@metral.ch>
2019-03-07 14:42:00 +0100

    Remove unused import

--- src/components/Cafe.js
+++ src/components/Cafe.js
@@ -2,34 +2,13 @@ import React, { Component } from "react";
 import { Box } from "grommet";
 import Info from "./Info";
 import Reviews from "./Reviews";
-import Heading from "./Heading";
 import Auth from "./Auth";
 import CreateCafe from "./CreateCafe";
 
 class Cafe extends Component {
-  state = {
-    createCafeOpen: false,
-    userOpen: false
-  };
-
-  toggleCreateCafe = () => {
-    const createCafeOpen = this.state.createCafeOpen ? false : true;
-    this.setState({ createCafeOpen });
-  };
-
-  toggleUser = () => {
-    this.setState({ userOpen: !this.state.userOpen });
-  };
-
   render() {
     return (
       <Box flex={false}>
-        <Heading
-          title={this.props.cafe.properties.name}
-          togglePanel={this.props.togglePanel}
-          toggleCreateCafe={this.toggleCreateCafe}
-          toggleUser={this.toggleUser}
-        />
         <Box>
           <Info cafe={this.props.cafe} />
         </Box>
@@ -43,7 +22,7 @@ class Cafe extends Component {
           />
         </Box>
         {/* TODO make User.js component with both Auth and CreateCafe buttons */}
-        {this.state.createCafeOpen && (
+        {this.props.createCafeOpen && (
           <CreateCafe
             createCafe={this.props.createCafe}
             toggleCreateCafe={this.toggleCreateCafe}
