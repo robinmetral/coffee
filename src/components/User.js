08f9a577ce37325360a322f7378331e420fd9349
Robin Métral <robin@metral.ch>
2019-03-07 20:32:56 +0100

    Transform User into a SFC

--- src/components/User.js
+++ src/components/User.js
@@ -1,48 +1,43 @@
-import React, { Component } from "react";
+import React from "react";
 import { Layer, Box, Heading, Button } from "grommet";
 import { ThemeContext } from "grommet/contexts";
 import { Close } from "grommet-icons";
 import Login from "./Login";
 import Logout from "./Logout";
 
-// TODO make a SFC
-class User extends Component {
-  render() {
-    return (
-      <ThemeContext.Extend
-        value={{
-          layer: {
-            zIndex: "1100",
-            container: {
-              zIndex: "1100"
-            }
-          }
-        }}
-      >
-        <Layer
-          onEsc={() => this.props.toggleUser()}
-          onClickOutside={() => this.props.toggleUser()}
-        >
-          <Box pad="medium" gap="small" width="medium">
-            <Box direction="row" align="center" justify="between">
-              <Heading level={3} margin="none">
-                {this.props.user
-                  ? `Hey ${this.props.user.displayName}!`
-                  : `Join the movement`}
-              </Heading>
-              <Button icon={<Close />} onClick={this.props.toggleUser} />
-            </Box>
-            {/* TODO display message here if user has opened the layer by attempting to write a review */}
-            {this.props.user ? (
-              <Logout logout={this.props.logout} />
-            ) : (
-              <Login login={this.props.login} />
-            )}
-          </Box>
-        </Layer>
-      </ThemeContext.Extend>
-    );
-  }
-}
+const User = () => (
+  <ThemeContext.Extend
+    value={{
+      layer: {
+        zIndex: "1100",
+        container: {
+          zIndex: "1100"
+        }
+      }
+    }}
+  >
+    <Layer
+      onEsc={() => this.props.toggleUser()}
+      onClickOutside={() => this.props.toggleUser()}
+    >
+      <Box pad="medium" gap="small" width="medium">
+        <Box direction="row" align="center" justify="between">
+          <Heading level={3} margin="none">
+            {this.props.user
+              ? `Hey ${this.props.user.displayName}!`
+              : `Join the movement`}
+          </Heading>
+          <Button icon={<Close />} onClick={this.props.toggleUser} />
+        </Box>
+        {/* TODO display message here if user has opened the layer by attempting to write a review */}
+        {this.props.user ? (
+          <Logout logout={this.props.logout} />
+        ) : (
+          <Login login={this.props.login} />
+        )}
+      </Box>
+    </Layer>
+  </ThemeContext.Extend>
+);
 
 export default User;
