55a21386745b99bea2d75786fbd2a7d78a486c66
Robin Métral <robin@metral.ch>
2019-03-10 15:07:20 +0100

    Transform Login into stateful component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,39 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import { Button, Text, Box } from "grommet";
 import { Github, Twitter, Facebook, Google } from "grommet-icons";
 
-const Login = props => {
-  const providers = [
-    {
-      name: "Github",
-      icon: <Github />
-    },
-    {
-      name: "Twitter",
-      icon: <Twitter />
-    },
-    {
-      name: "Facebook",
-      icon: <Facebook />
-    },
-    {
-      name: "Google",
-      icon: <Google />
-    }
-  ];
+class Login extends Component {
+  render() {
+    const providers = [
+      {
+        name: "Github",
+        icon: <Github />
+      },
+      {
+        name: "Twitter",
+        icon: <Twitter />
+      },
+      {
+        name: "Facebook",
+        icon: <Facebook />
+      },
+      {
+        name: "Google",
+        icon: <Google />
+      }
+    ];
 
-  return (
-    <Box direction="row" align="center">
-      <Text>Log in with:</Text>
-      {providers.map(({ name, icon }) => (
-        <Button
-          plain
-          key={name}
-          icon={icon}
-          onClick={() => props.login(name)}
-        />
-      ))}
-    </Box>
-  );
-};
+    return (
+      <Box direction="row" align="center">
+        <Text>Log in with:</Text>
+        {providers.map(({ name, icon }) => (
+          <Button
+            plain
+            key={name}
+            icon={icon}
+            onClick={() => props.login(name)}
+          />
+        ))}
+      </Box>
+    );
+  }
+}
 export default Login;
